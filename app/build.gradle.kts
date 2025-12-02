f22501670b1768a787e4c024402fcb4bb5a201bb
SlivA54 <gavrisevdanil765@gmail.com>
2025-12-03 00:28:31 +0700

    2

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -66,4 +66,9 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation("androidx.compose.ui:ui:1.6.4")
+    implementation("androidx.compose.ui:ui-tooling-preview:1.6.4")
+    implementation("androidx.compose.material3:material3:1.2.1")
+    implementation("androidx.activity:activity-compose:1.9.0")
+
 }
